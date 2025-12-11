c254cece92d0ebb28a9645b6ad53d56072325384
Yujung20 <ybmdeer1@gmail.com>
2025-12-11 11:40:49 +0900

    made nav bar

--- conf/urls.py
+++ conf/urls.py
@@ -19,8 +19,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from conf.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("",index),
 ]
