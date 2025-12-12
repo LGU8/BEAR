bf6878ee452c92f5f55f6e77501a7ad1924f14de
Yujung20 <ybmdeer1@gmail.com>
2025-12-12 14:26:44 +0900

    change nav text color

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-&1hu5v_erep=*)if6z8#epr=c3&cug5yd*_frw=3_dk95yc24r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
