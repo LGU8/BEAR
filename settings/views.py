ca861d8c3595ab3bda6e725997ae53b367f52e8a
janghjun <yyuyiy@naver.com>
2025-12-16 11:24:41 +0900

    settings / badge 파일 구조 변경

--- settings/views.py
+++ settings/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def settings_index(request):
-    return render(request, "settings_index.html")
+    return render(request, "settings/settings_index.html")
