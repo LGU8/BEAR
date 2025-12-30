351f86961b7597f0eac262f5a2a0e7d9a99a4ca1
janghjun <yyuyiy@naver.com>
2025-12-30 16:44:41 +0900

    push1

--- Projects/accounts/urls.py
+++ Projects/accounts/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("login/", views.user_login, name="user_login"),
     path("logout/", views.user_logout, name="user_logout"),
 
-<<<<<<< Updated upstream
     # password reset (중복 제거, name 유지)
     path("password-reset/", views.password_reset, name="password_reset"),
     path("password-reset-confirm/<uidb64>/<token>/", views.password_reset_confirm, name="password_reset_confirm"),
@@ -17,15 +16,7 @@ urlpatterns = [
     path("signup_step2/", views.signup_step2, name="signup_step2"),
     path("signup_step3/", views.signup_step3, name="signup_step3"),
     path("signup_step4/", views.signup_step4, name="signup_step4"),
-=======
-    path("password-reset/", views.password_reset, name="password_reset"),
-    path("password-reset-confirm/<uidb64>/<token>/", views.password_reset_confirm, name="password_reset_confirm"),
 
-    path("signup/step1/", views.signup_step1, name="signup_step1"),
-    path("signup/step2/", views.signup_step2, name="signup_step2"),
-    path("signup/step3/", views.signup_step3, name="signup_step3"),
-    path("signup/step4/", views.signup_step4, name="signup_step4"),
->>>>>>> Stashed changes
 
     path("profile/", views.profile, name="profile"),
     path("test-login/", views.test_login_view, name="test_login"),
