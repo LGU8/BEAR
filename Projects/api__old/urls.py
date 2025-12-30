c6fda8f741fa39d7cd9b19b9d62edba7c17a51b2
knk010203 <knk010203@gmail.com>
2025-12-30 17:11:33 +0900

    Remove *_old backup directories under Projects

--- Projects/api__old/urls.py
+++
@@ -1,8 +0,0 @@
-# api/urls.py
-from django.urls import path
-from .views import menu_recommend_post, menu_recommend_get
-
-urlpatterns = [
-    path("menu/recommend", menu_recommend_post, name="menu_recommend_post"),  # POST
-    path("menu/recommend/", menu_recommend_get, name="menu_recommend_get"),  # GET (끝 슬래시 허용)
-]
--- Projects/ml__old/menu_reco/common/types.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-from dataclasses import dataclass
-from typing import Tuple, Dict, Optional, Iterable, Set, List
-
-Context = Tuple[str, str]
