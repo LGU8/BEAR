ede0ccdd7538c1e9a758dfe7cd5e076f5a217d65
janghjun <yyuyiy@naver.com>
2025-12-31 14:09:28 +0900

    accounts

--- Projects/conf/urls.py
+++ Projects/conf/urls.py
@@ -2,28 +2,34 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from conf import views as conf_views
+from record.views import timeline
+from accounts.views import user_login
 from conf import views as conf_views
 
-def root_to_home(request):
-    return redirect("home")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    # 루트 주소 접속 시 home으로 우회
-    path("", root_to_home, name="root"),
-
-    # 홈
+    # 루트 주소 접속 시 로그인 페이지
+    path("", user_login, name="root"),
+    # 로그인 후 이동할 홈
     path("home/", conf_views.index, name="home"),
-
-    # 앱 include
-    path("accounts/", include(("accounts.urls", "accounts_app"), namespace="accounts_app")),
+    # 앱 단위 include
+    path(
+        "accounts/",
+        include(("accounts.urls", "accounts_app"), namespace="accounts_app"),
+    ),
     path("record/", include("record.urls")),
     path("settings/", include("settings.urls")),
-    path("report/", include("report.urls")),
-
-    # badges
+    # 뷰 함수 직접 연결 (데코레이터가 붙은 conf_views의 함수인지 확인!)
+    path("timeline/", timeline, name="timeline"),
     path("badges/", conf_views.badges_redirect, name="badges"),
+    # report가 include 방식이라면, 해당 앱의 views.py에도 @login_required가 있어야 합니다.
+    path("report/", include("report.urls")),
 ]
 
 try:
