d8edd87cd3af7428457ade3dd57f4f78173ef868
MJ-Kim <mj.elisa531@gmail.com>
2026-01-05 11:10:22 +0900

    2026.01.05 LLM(RAG) file upload

---
+++ LLM/menu_RAG/build_menu_vector_store.py
@@ -0,0 +1,53 @@
+from langchain_community.document_loaders import PyPDFLoader, UnstructuredURLLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from dotenv import load_dotenv
+import os
+
+rag_path = "./menu_RAG"
+all_splits = []
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+
+# pdf 불러오기 및 청크화
+for filename in os.listdir(rag_path):
+    if filename.endswith(".pdf"):
+        input_path = os.path.join(rag_path, filename)
+        
+        try:
+            loader = PyPDFLoader(input_path)
+            docs = loader.load()
+
+            # 문서에 metadata 추가
+            for d in docs:
+                d.metadata["source_file"] = filename
+
+            splits = text_splitter.split_documents(docs)
+            all_splits.extend(splits)
+
+        except Exception as e:
+            print(f"[ERROR] PDF 로딩 실패: {input_path}: {e}")
+
+
+print(f"총 청크 수: {len(all_splits)}")
+
+# Embedding
+load_dotenv("./Chain/.env")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+# test_vector_result = embeddings.embed_query("부정적인 감정일 때, 기분 개선에 효과적인 음식은 뭐야?")
+# print(test_vector_result)
+# print(f"임베딩 벡터 차원 수: {len(test_vector_result)}")
+
+# VectorDB
+persist_directory = "./Chain/menu_chroma_store"
+if not os.path.exists(persist_directory):
+    vectorstore = Chroma.from_documents(
+        documents=all_splits, embedding=embeddings, persist_directory=persist_directory
+    )
+else:
+    vectorstore = Chroma(
+        embedding_function=embeddings, persist_directory=persist_directory
+    )
+print("저장된 벡터 개수:", vectorstore._collection.count())
